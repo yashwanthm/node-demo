997268132af45446c7b5998780b78dff91ea8327
Yashwanth Maheshwaram <yashwanth@redhat.com>
2023-11-02 19:55:33 +0530

    hey world

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/hey', (req, res) => {
+    res.send('Hey world!')
+  })
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

6ffead8987b06c2aad9999884b41577d4ae957bd
Yashwanth Maheshwaram <yashwanth@redhat.com>
2023-12-03 22:09:12 +0530

    test

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const express = require('express')
-const app = express()
+import Express from 'express'
+import PG  from 'pg'
+const client = new PG.Client()
+ 
+const app = new Express()
 const port = 8080
 
 app.get('/', (req, res) => {
+
   res.send('Hello world!')
+
+  // client.connect((err) => {
+  //   client.query('SELECT $1::text as message', ['Hello world!'], (err, data) => {
+  //     console.log(err ? err.stack : data.rows[0].message) // Hello World!
+  //     res.send('Hello world!')
+  //     client.end()
+  //   })
+  // })
+  
 })
   
 app.listen(port, () => {

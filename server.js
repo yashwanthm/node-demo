e810b7a77f0b16633150264e38170552faa9d97c
Yashwanth Maheshwaram <yashwanth@redhat.com>
2023-12-04 10:18:47 +0530

    clean

--- server.js
+++ server.js
@@ -6,17 +6,7 @@ const app = new Express()
 const port = 8080
 
 app.get('/', (req, res) => {
-
   res.send('Hello world!')
-
-  // client.connect((err) => {
-  //   client.query('SELECT $1::text as message', ['Hello world!'], (err, data) => {
-  //     console.log(err ? err.stack : data.rows[0].message) // Hello World!
-  //     res.send('Hello world!')
-  //     client.end()
-  //   })
-  // })
-  
 })
   
 app.listen(port, () => {
